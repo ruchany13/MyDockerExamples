55e09219f1e449dacc52fb99dc4cc5a0c57cd190
Ruchan Yalçın <ruchany13@gmail.com>
2024-12-25 12:39:22 +0300

    deleted basic examples, it will rewrite

--- basic_flask/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hey, we have Flask in a Docker Container"
-
-if __name__ =='__main__':
-    app.run(debug=True, host='0.0.0.0')
